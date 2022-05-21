b29c34aa3836a7a459e2eb066c0d77138ba65912
unknown <yumiiiiiii>
2022-05-21 23:30:23 +0900

    bootstrap

--- Dblogproject/blogproject/urls.py
+++ Dblogproject/blogproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blog.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +31,6 @@ urlpatterns = [
 
     path('<int:blog_id>/comment',blog.views.add_comment, name="add_comment"),
 
-    path('<int:blog_id>/comment/<int:comment_id>/delete/',blog.views.delete_comment, name="delete_comment")
+    path('<int:blog_id>/comment/<int:comment_id>/delete/',blog.views.delete_comment, name="delete_comment"),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
